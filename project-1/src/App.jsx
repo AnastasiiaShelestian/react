809412a7ffac0f1cbb7d683f330ff06e457204af
AnastasiiaShelestian <shelestnest@gmail.com>
2025-02-10 12:12:44 +0200

    The first project

---
+++ project-1/src/App.jsx
@@ -0,0 +1,14 @@
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const name = "Vite";
+
+  return (
+    <div>
+      <img src={viteLogo} />
+    </div>
+  );
+}
+
+export default App;
